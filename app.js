a9744bf20745051df48716669fa6853db355a22d
Joseph LoPresti <josephlopresti.student@careerdevs.com>
2019-01-20 20:11:52 +0000

    Version 1 of hotel updated

--- app.js
+++ app.js
@@ -2,18 +2,21 @@ var express = require("express");
 var app = express();
 
 
-app.get("/", function(req, res) {
-   res.send("<h1>Welcome to the homepage!</h1>"); 
+app.get("/home", function(req, res) {
+   res.render("home.ejs");
+});
+
+app.get("/fallinlovewith/:thing", function(req, res){
+   var thing = req.params.thing;
+   
+   res.render("love.ejs", {thingVar: thing});
 });
 
 
 
 
 
-
-
-
-
+// listen for requests
 app.listen(process.env.PORT, process.env.IP, function() {
    console.log("Server is running"); 
 });
